c8b8cbabbf326182e530b065c15ebdbce2364c2c
BOISNIER Thomas <thomas.boisnier@etud.univ-jfc.fr>
2019-04-04 17:29:18 +0200

    Adding data fixtures (defibrillators).

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,9 +2,11 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Defibrillator;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture {
@@ -17,20 +19,46 @@ class AppFixtures extends Fixture {
 
     public function load(ObjectManager $manager) {
 
-        $faker = \Faker\Factory::create('en_US');
+        $faker = Factory::create('en_US');
 
-        for ($i = 1; $i <= 30; ++$i) {
-            $user = new User();
-            $user->setEmail($faker->email);
-            $user->setPassword(
-                $this->_passwordEncoder->encodePassword($user,'1234')
-            );
-            $user->setRoles(['ROLE_USER']);
-            $manager->persist($user);
+        // Generate random users
+//        for ($i = 1; $i <= 30; ++$i) {
+//            $user = new User();
+//            $user->setEmail($faker->email);
+//            $user->setPassword(
+//                $this->_passwordEncoder->encodePassword($user,'1234')
+//            );
+//            $user->setRoles(['ROLE_USER']);
+//            $manager->persist($user);
+//
+//            $manager->flush();
+//        }
 
+        // Load defibrillators from OpenData files
+        $jsonData = json_decode(
+            file_get_contents(
+                "https://data.toulouse-metropole.fr/explore/dataset/defibrillateurs/download/?format=json&timezone=Europe/Berlin"
+            ),
+            true
+        );
 
+        foreach ($jsonData as $data) {
+            $defibrillator = new Defibrillator();
+            $defibrillator->setLongitude($data['fields']['geo_point_2d'][1]);
+            $defibrillator->setLatitude($data['fields']['geo_point_2d'][0]);
 
-            $manager->flush();
+            $implantation = isset($data['fields']['implantation']) ? $data['fields']['implantation'] : '';
+
+            $note = $data['fields']['nom_site'] ."<br />".
+                $implantation .
+                $data['fields']['adresse'] ."<br />".
+                $data['fields']['accessibilite'];
+
+            $defibrillator->setNote($note);
+
+            $manager->persist($defibrillator);
         }
+
+        $manager->flush();
     }
 }
